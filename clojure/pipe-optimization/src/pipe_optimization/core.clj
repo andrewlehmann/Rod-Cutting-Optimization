811e2a50f748ca3042265511808153485bb2a4b4
andrewlehmann <drewlehmann1@yahoo.com>
2017-11-20 20:38:22 -0600

    refactor clojure solution slightly

--- clojure/pipe-optimization/src/pipe_optimization/core.clj
+++ clojure/pipe-optimization/src/pipe_optimization/core.clj
@@ -3,13 +3,13 @@
   (:gen-class))
 
 (defn get-cuts
-  [length prices]
+  [length inventory]
   (sort-by first
     (filter 
       #(= (reduce + 0 %) length)
       (combo/subsets
         (apply concat
-          (filter (fn[e] (every? #(contains? prices %) e)) 
+          (filter (fn[e] (every? #(contains? inventory %) e)) 
           (map 
             #(repeat (- length (- % 1)) %)
             (range 1 (+ length 1)))))))))
@@ -29,15 +29,12 @@
       (if (> i length)
         (apply hash-map (interleave [:max-price :cuts] (cache length)))
         (recur
-         (let [cuts (if (< i 2)
-                      (if (< i 1) nil [[1]])
-                      (get-cuts i inventory))]
-           (let [prices (map #(get-price-of-cuts i % cache inventory) cuts)
-                 my-max (reduce max prices)]
-             (merge cache
-                    {i
-                     [my-max,
-                      (filter
-                       #(= (get-price-of-cuts i % cache inventory) my-max)
-                       cuts)]})))
+         (let [cuts (get-cuts i inventory)]
+           (let [ max-price (reduce max (map #(get-price-of-cuts i % cache inventory) cuts))]
+             (merge 
+              cache
+              {i
+                [max-price,
+                (filter #(= (get-price-of-cuts i % cache inventory) max-price) 
+                cuts)]})))
          (+ i 1))))))
