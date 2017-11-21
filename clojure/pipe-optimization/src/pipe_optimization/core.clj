b89d3b52d06ca5d94ff8c235f3cf8c9540683052
andrewlehmann <drewlehmann1@yahoo.com>
2017-11-20 21:01:50 -0600

    reorder lines for readability

--- clojure/pipe-optimization/src/pipe_optimization/core.clj
+++ clojure/pipe-optimization/src/pipe_optimization/core.clj
@@ -5,16 +5,13 @@
 (defn get-cuts
   [length inventory]
   (sort-by first
-    (filter 
-      #(= (reduce + 0 %) length)
+    (filter #(= (reduce + 0 %) length)
       (combo/subsets
         (apply concat
           (filter (fn[e] (every? #(contains? inventory %) e)) 
-          (map 
-            #(repeat (- length (- % 1)) %)
-            (range 1 (+ length 1)))))))))
+          (map #(repeat (- length (- % 1)) %) (range 1 (+ length 1)))))))))
 
-(defn get-price-of-cuts
+(defn get-price
   [length cuts results inventory]
   (if (contains? inventory length)
     (reduce + 0 (map inventory cuts))
@@ -23,18 +20,15 @@
 (defn max-price
   "takes a desired length and a hash map of inventory"
   [length inventory]
-  (if (= length 0)
-    {:max-price 0 :cuts [[]]} 
+  (if (= length 0) {:max-price 0 :cuts [[]]} 
     (loop [cache {} i 1]
-      (if (> i length)
+      (if (> i length) 
         (apply hash-map (interleave [:max-price :cuts] (cache length)))
-        (recur
-         (let [cuts (get-cuts i inventory)]
-           (let [ max-price (reduce max (map #(get-price-of-cuts i % cache inventory) cuts))]
-             (merge 
-              cache
-              {i
-                [max-price,
-                (filter #(= (get-price-of-cuts i % cache inventory) max-price) 
-                cuts)]})))
+        (recur 
+          (let [cuts (get-cuts i inventory)
+                my-max (reduce max (map #(get-price i % cache inventory) cuts))]
+            (merge 
+              cache 
+              {i [my-max, 
+                  (filter #(= (get-price i % cache inventory) my-max) cuts)]}))
          (+ i 1))))))
