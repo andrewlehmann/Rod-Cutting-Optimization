97f4f896a7391552e362e1e17193db2a94b2f8b8
Andrew Lehmann <andrew@arlehmann.com>
2017-12-05 14:51:06 -0600

    add test for 5

--- haskell/test/MaxPriceTest.hs
+++ haskell/test/MaxPriceTest.hs
@@ -25,6 +25,9 @@ main = hspec $
     it "max-price for 4 length" $
       maxPrice 4 (Map.fromList [(1, 1), (2, 2), (3, 3), (4, 2)]) `shouldBe` (4, [[1,1,1,1], [1,1,2], [1, 3], [2, 2]])
 
+    it "max-price for 5 length" $
+      maxPrice 5 (Map.fromList [(1, 1), (2, 2), (3, 3), (4, 2)]) `shouldBe` (5, [[1,1,1,1,1], [1,1,1,2], [1,1,3], [1,2,2], [1,4], [2, 3]])
+
     it "max-price for 17 length" $
       (length (snd result) == 33) && (fst result == 17)
         where
